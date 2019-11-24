963f520bd72e0d04abf55ab40e5935982ff796b7
paz <chaim@webiks.com>
2019-11-24 20:19:37 +0200

    new Scheduler class

--- app/cron/scheduler.py
+++ app/cron/scheduler.py
@@ -1,22 +1,28 @@
-import time
 import logging
-from timeloop import Timeloop
-from datetime import timedelta
-
-logger = logging.getLogger(__name__)
-# run a task in a set interval
-tl = Timeloop()
+from threading import Timer
 
 
-def start_cron(period):
-    @tl.job(interval=timedelta(seconds=period))
-    def sample_job_every_2s():
-        data = (str(period) + " sec job current time : {}").format(time.ctime())
-        logger.info(data)
-        # integrate the app you want to run
+class Scheduler(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
 
-    tl.start()  # block=True
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
 
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
 
-def stop_cron():
-    tl.stop()
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
