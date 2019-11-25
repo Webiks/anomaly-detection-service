b711e3239fe272f61070a7fc1465955104b85c1f
paz <chaim@webiks.com>
2019-11-25 23:00:50 +0200

    load model from ./data

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,20 @@
 import os
 import sys
 import logging
+from joblib import load
 from flask import Flask
 from dotenv import load_dotenv
 from app.config import Config
 from app.cron.scheduler import Scheduler
-from app.ml.model import load
+from app.handlers.predict_handler import run_predict, run_dry
+from logging.handlers import RotatingFileHandler
 
-cfg = Config().cfg
+cfg = Config.getInstance().cfg
 
 logger = logging.getLogger(__name__)
 logger.setLevel(cfg.logger.logLevel)
 
-fh = logging.FileHandler(cfg.logger.filename)
+fh = RotatingFileHandler(cfg.logger.filename, mode='a', maxBytes=5*1024*1024, backupCount=2, encoding=None, delay=0)
 fh.setLevel(cfg.logger.logLevel)
 
 ch = logging.StreamHandler(sys.stdout)
@@ -25,13 +27,15 @@ ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
+logger.info(cfg)
 app = Flask(__name__)
-
-app.rt = Scheduler(cfg.cron.setInterval, load, "Elastic")
-logger.info(app.rt)
 
 load_dotenv()
 app.webhook_url = os.getenv("WEBHOOKURL")
+
+app.rt = Scheduler(cfg.cron.setInterval, run_dry, "Elastic")
+
+app.isfr_model = load(cfg.model.pathname)
 
 from app import routes
 
