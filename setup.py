52a3634fdd7c8efd9f68344dc816e61773c607f8
paz <chaim@webiks.com>
2019-12-03 12:03:39 +0200

    init commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Anomaly Detection Service',
-    version='0.1.0',
+    version='0.1.1',
     packages=[],
     license='MIT',
     description='anomaly detection service',
